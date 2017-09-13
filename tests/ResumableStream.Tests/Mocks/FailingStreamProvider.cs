e3a52b13486b7603ba0aa3aece45ef10720ebbb1
Vladimir Tyrin <b0-0b@yandex.ru>
2017-09-13 18:15:31 +0300

    Mocks

---
+++ tests/ResumableStream.Tests/Mocks/FailingStreamProvider.cs
@@ -0,0 +1,51 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using ResumableStream.Tests.Exceptions;
+
+namespace ResumableStream.Tests.Mocks
+{
+	/// <summary>
+	///		Fails after first return
+	/// </summary>
+	public class FailingStreamProvider : IStreamProvider
+	{
+		public FailingStreamProvider(Stream stream, bool recoverable)
+		{
+			_stream = stream;
+			_recoverable = recoverable;
+		}
+
+		public Stream GetStream(long position)
+		{
+			if (_shouldFail)
+				Fail();
+
+			_shouldFail = true;
+			return _stream;
+		}
+
+		public Task<Stream> GetStreamAsync(long position, CancellationToken cancellationToken)
+		{
+			if (_shouldFail)
+				Fail();
+
+			_shouldFail = true;
+			return Task.FromResult(_stream);
+		}
+
+		private void Fail()
+		{
+			if (_recoverable)
+				throw new RecoverableException();
+			throw new UnrecoverableException();
+		}
+
+		private bool _shouldFail;
+		private readonly Stream _stream;
+		private readonly bool _recoverable;
+	}
+}
